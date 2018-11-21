aeb62fd99bbeecc3821218993ae3284bc2bd262d
tiwariShubhi <shubhitiwari23@gmail.com>
2018-11-22 00:13:52 +0530

    merged file created

---
+++ code/mergeData.py
@@ -0,0 +1,17 @@
+import os
+
+folderPath = 'D:/m.tech/Sem 3/NLP/Project/VideoIndexing/Dataset/COURSERA_BigData/'
+outFile = 'D:/m.tech/Sem 3/NLP/Project/VideoIndexing/Result/BigData.vdtt'
+dirList = os.listdir(folderPath)
+
+fp = open(outFile,'w')
+for dirName in dirList:
+    dir2 = os.listdir(folderPath+dirName)
+    for d in dir2:
+        fileList = os.listdir(folderPath+dirName+'/'+d)
+        for file in fileList:
+            fileData = [line.rstrip('\n') for line in open(folderPath + dirName +'/'+ d+ '/' + file)]
+            for line in fileData:
+                fp.write(line+'\n')
+
+fp.close()
